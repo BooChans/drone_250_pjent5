ec6f27540e1502774068216487f596ecd1c65644
BooChans <164491081+BooChans@users.noreply.github.com>
2024-12-13 15:23:51 +0100

    Delete drone_250_pjent5 directory

--- drone_250_pjent5/telemetrie_test/detection.py
+++
@@ -1,6 +0,0 @@
-#ce programme liste les composants connectés en série à l'ordinateur (PC ou Raspberry)
-import serial.tools.list_ports
-print("Available serial ports:")
-ports = serial.tools.list_ports.comports()
-for port in ports:
-    print(port)
